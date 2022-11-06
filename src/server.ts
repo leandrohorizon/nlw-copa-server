3a1c3d7f77e2719a2324da475a8f1c3c0a44ee09
leandrohorizon <leandro.mcontato@gmail.com>
2022-11-05 22:39:26 -0300

    Rotas para contagens

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -15,10 +17,42 @@ async function bootstrap() {
     origin: true,
   })
 
-  fastify.get('/pools/count', () => {
-    const count = prisma.pool.count()
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
 
-    return { count: count }
+    return { count }
+  })
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const create_pool_body = z.object({
+      title: z.string()
+    })
+
+    const { title } = create_pool_body.parse(request.body)
+
+    const generated_code = new ShortUniqueId({ length: 6 })
+    const code = String(generated_code()).toUpperCase()
+
+    const pool = await prisma.pool.create({
+      data: {
+        title,
+        code
+      },
+    })
+
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({ host: '0.0.0.0', port: 3333 })
